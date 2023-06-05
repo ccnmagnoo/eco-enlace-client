8e28a3ef1c56068481af3a7e2522bd287b22ea25
ccnmagnoo <crcamposn@gmail.com>
2023-06-05 16:46:15 -0400

    evalpo reference

--- src/components/layout/bottombar.rs
+++ src/components/layout/bottombar.rs
@@ -8,7 +8,21 @@ pub struct Props {
 pub fn BottomBar(props: &Props) -> Html {
     html! {
         <footer>
-        {for props.children.iter()}
+            <section>
+                {for props.children.iter()}
+            </section>
+        <section class="auspices">
+            <span>
+                {"auspicio"}
+            </span>
+            <a href="https://www.facebook.com/EnergiaValpo/" target="_blank">
+            <span>
+                {"@energíaValpo"}
+            </span>
+            <img id="energia_valpo_logo" src="https://scontent.fscl3-1.fna.fbcdn.net/v/t39.30808-6/329003534_1492480671276258_2819404882572271341_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=knn0ryWA43sAX9tOvJi&_nc_ht=scontent.fscl3-1.fna&oh=00_AfD3NhwoM3lXiyflJAkwDaG34rFqCZQM4j95fGkWkaQsYg&oe=6483B2FC" alt="energia_logo" />
+            </a>
+
+        </section>
         </footer>
     }
 }
